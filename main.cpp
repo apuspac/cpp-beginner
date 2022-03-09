6938849b25d13a7504d9e51b3f387f58cd0829f7
TAKE_PC_WSL <penginstar1299@gmail.com>
2022-03-09 18:46:45 +0900

    [udpate] add class code
    クラスの追加

--- main.cpp
+++ main.cpp
@@ -1,32 +1,18 @@
-#include "all.h"
+struct fractional
+{
+    int num;
+    int denom;
+
+    fractional(int num)
+        : num(num), denom(1)
+    {
+    }
+};
 
 int main()
 {
-    std::vector<int> v = {1, 2, 3, 4, 5};
+    fractional a = 1;
+    fractional b = 2;
 
-    // 奇数の数: 5
-    auto a = std::count_if( std::begin(v), std::end(v),
-        [](auto x){ return x%2 == 1 ; } ) ;
-
-    // 偶数の数: 1
-    auto b = std::count_if( std::begin(v), std::end(v),
-        [](auto x){ return x%2 == 0 ; } ) ;
-
-    // 2以上の数: 3
-    auto c = std::count_if( std::begin(v), std::end(v),
-        [](auto x){ return x >= 2 ; } ) ;
-
-    auto count_if = [](auto first, auto end, auto pred){
-        auto counter = 0u;
-
-        for(auto iter = first; iter != end; ++iter){
-            if(pred(*iter) != false){
-                counter++;
-            }
-        }
-        return counter;
-    };
-
-    std::cout << count_if(v.begin(), v.end(), [](auto x){return x%2 == 1;});
-
+    std::cout << a << " " << b << "\n";
 }
